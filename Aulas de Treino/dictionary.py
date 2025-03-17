8fa248a29a0691cd092a017b5db3c2fa93ada961
WagnerSJ-Atlas <wagner.silva@atlasinovacoes.com.br>
2025-03-17 17:10:50 -0300

    feat: list comprehension e Expressão generator.

---
+++ Aulas de Treino/dictionary.py
@@ -0,0 +1,9 @@
+# Dicionario python
+'''
+aluno = {'nome': 'Ana', "idade": 16}
+'''
+# loop para retornar values
+
+aluno = {'nome': 'Ana', "idade": 16}
+for keys, values in aluno.items():
+    print(keys, values)
--- Aulas de Treino/list.py
+++ Aulas de Treino/list.py
@@ -16,13 +16,13 @@ print(frutas_lista)
 
 # Tupla (a diferença de tupla é lista é q a tupla é imutavel)
 # Tupla gasta menos memoria! 
-
+'''
 cores_list = ['amarelo', 'verde', 'azul']
 cores_tupla = ('amarelo', 'verde', 'azul')
-
+'''
 # Existe os SETS tbm que EVITA itens duplicados
 # Sets não utilizam index
-
+'''
 list1 = [10, 20, 30, 50, 60]
 list2 = [10, 20, 30, 40, 70]
 
@@ -30,3 +30,24 @@ num1 = set(list1)
 num2 = set(list2)
 
 print(num1)
+'''
+
+# list comprehension
+    # Para criar uma nova lista a partir de uma existente
+    # expressao for iten in itens
+
+frutas1 = ['maça', 'abacaxi', 'limao', 'morango', 'abacate']
+
+# frutas2 = []
+
+# for iten in frutas1:
+#     if 'b' in iten:
+#         frutas2.append(iten)
+
+frutas2 = [iten for iten in frutas1 if 'b' in iten]
+
+print(frutas2)
+
+# Expressões generator (MUITO MELHOR PARA DESEMEPNHO)
+
+'Para fazer um generator é só mudar o [] de uma lista para ().'
